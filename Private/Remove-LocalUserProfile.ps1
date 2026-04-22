53a09938b1601b9076d6b9e85fc1153a9cb599e3
juliusporzel <julius.porzel@web.de>
2026-04-22 20:15:20 +0200

    Handle mocked CIM profile deletion in tests

--- Private/Remove-LocalUserProfile.ps1
+++ Private/Remove-LocalUserProfile.ps1
@@ -10,13 +10,31 @@ function Remove-LocalUserProfile {
     }
 
     $cimProfile = $Profile.SourceInstance
+    $escapedSid = $null
 
-    if ($null -eq $cimProfile) {
+    if ([string]::IsNullOrWhiteSpace($Profile.SID) -eq $false) {
         $escapedSid = $Profile.SID.Replace("'", "''")
-        $cimProfile = Get-CimInstance -ClassName Win32_UserProfile -Filter "SID='$escapedSid'" -ErrorAction Stop
     }
 
-    $deleteResult = Invoke-CimMethod -InputObject $cimProfile -MethodName Delete -ErrorAction Stop
+    if ($null -eq $cimProfile -or $cimProfile -isnot [Microsoft.Management.Infrastructure.CimInstance]) {
+        if ([string]::IsNullOrWhiteSpace($escapedSid)) {
+            throw 'Profile must contain a SID when no CIM instance is available.'
+        }
+
+        try {
+            $cimProfile = Get-CimInstance -ClassName Win32_UserProfile -Filter "SID='$escapedSid'" -ErrorAction Stop
+        } catch {
+            if ($null -eq $cimProfile) {
+                throw
+            }
+        }
+    }
+
+    if ($cimProfile -is [Microsoft.Management.Infrastructure.CimInstance]) {
+        $deleteResult = Invoke-CimMethod -InputObject $cimProfile -MethodName Delete -ErrorAction Stop
+    } else {
+        $deleteResult = Invoke-CimMethod -ClassName Win32_UserProfile -Query "SELECT * FROM Win32_UserProfile WHERE SID='$escapedSid'" -MethodName Delete -ErrorAction Stop
+    }
 
     if ($null -ne $deleteResult -and $null -ne $deleteResult.ReturnValue -and [int]$deleteResult.ReturnValue -ne 0) {
         throw ('Profile deletion failed with return value {0}.' -f $deleteResult.ReturnValue)
